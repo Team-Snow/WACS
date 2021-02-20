a7951de20ab11fff16f1a5a095bb148949d7aea8
Jack Pilley <boxedvideos@gmail.com>
2021-02-20 15:05:22 -0800

    Started work on website to show status of tasks being worked on

---
+++ src/Entry.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let taskName: string;
+    export let timesServed: number;
+    export let timesCompleted: number;
+    export let leadingSolution: string;
+</script>
+
+<tr>
+    <td>{taskName}</td>
+    <td>{timesServed}</td>
+    <td>{timesCompleted}</td>
+    <td>{leadingSolution}</td>
+</tr>
+
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import App from './App.svelte';
+import Entry from './Entry.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+const tasks: Entry[] = [];
+
+for(let i = 0; i < 5; i++)
+{
+	tasks.push(new Entry({
+		target: document.querySelector("#work-table"),
+		props: {
+			taskName: `${i}`,
+			timesServed: 0,
+			timesCompleted: 0,
+			leadingSolution:  "[Pending]"
+		}
+	}));
+}
 
 export default app;
