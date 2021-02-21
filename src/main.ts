9760389228e2830a5f03a07b3e04270f0076f41b
Jack Pilley <boxedvideos@gmail.com>
2021-02-21 01:35:53 -0800

    Tasks are now named using their hash

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ for(let i = 0; i < 5; i++)
 	tasks.push(new Entry({
 		target: document.querySelector("#work-table"),
 		props: {
-			taskName: `${i}`,
+			taskName: "...",
 			currentWorkers: 0,
 			timesServed: 0,
 			timesCompleted: 0,
@@ -24,13 +24,19 @@ socket.addEventListener('open', () => {
 	console.log("Opened client connection.")
 });
 
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+	return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
+}
+
 socket.addEventListener('message', (e) => {
 	let message = JSON.parse(e.data);
-	console.log(message);
+	//console.log(message);
 	let hashes = message.hashes;
 	for(let i = 0; i < hashes.length && i < tasks.length; i++)
 	{
+		//console.log(buf2hex(hashes[i].hash.slice(0,3)))
 		tasks[i].$$set({
+			taskName: buf2hex(hashes[i].hash.slice(0,3)) + "...",
 			timesServed: hashes[i].served,
 			currentWorkers: hashes[i].nodes.length,
 			timesCompleted: hashes[i].completed,
